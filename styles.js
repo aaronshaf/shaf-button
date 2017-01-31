d4225a27adf55892b2f6b883c0f86c50de6c891b
Aaron Shafovaloff <aaronshaf@gmail.com>
2017-01-31 10:19:19 -0700

    more mobile-friendly

--- styles.js
+++ styles.js
@@ -2,8 +2,11 @@ export default `
 .shaf-button {
   transition: transform 0.05s;
   transform: rotateX(0deg) rotateY(0deg);
-  user-select: none;
   will-change: transform;
+  user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
   -webkit-transform: translateZ(0px);
   -webkit-font-smoothing: subpixel-antialiased;
 }`
